b9334eb1e3ba8842c9c86c854dc863f5708b6dca
viral rupani <viralrupani12017@gmail.com>
2024-10-11 01:06:45 +0530

    feat: user details

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CONSTANTS } from 'src/common/constants';
+import { IGenericResult } from 'src/common/interfaces';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(UsersEntity)
+        private readonly usersRepo: Repository<UsersEntity>,
+    ) { }
+
+    async getProfileDetails(user: any): Promise<IGenericResult> {
+        const foundUser: UsersEntity = await this.usersRepo.findOne({
+            where: {
+                id: user?.id,
+            },
+            select: {
+                id: true,
+                created_at: true,
+                email: true,
+                first_name: true,
+                is_verified_email: true,
+                last_name: true,
+                password: true,
+                phone_number: true,
+                profile_url: true,
+                role: true,
+                updated_at: true
+            },
+        });
+        if (!foundUser) {
+            throw new NotFoundException(CONSTANTS.USER_NOT_EXIST);
+        }
+        return {
+            message: 'User',
+            data: {
+                user: foundUser
+            }
+        };
+    }
+}
